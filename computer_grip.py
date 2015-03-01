1e6226506219600b352961a7cbb43216bc3320dc
CamelBackNotation <joshuahurt@utexas.edu>
2015-03-01 01:07:52 -0600

    Added ability to open spotify with a thumbs up

--- computer_grip.py
+++ computer_grip.py
@@ -1,8 +1,15 @@
 import sys
+import subprocess
 import os
 
+def spotify():
+    os.system("spotify")
+
 def thumbs_up():
-    os.system("snake-solver")
+    a = subprocess.check_output("pgrep -af python", shell=True)
+    if not "start.py" in a:
+        os.system("spotify &")
+
 
 def placeholder():
     print "Hello!"
@@ -10,7 +17,7 @@ def placeholder():
 gestures = {
             0 : placeholder,
             1 : placeholder,
-            2 : placeholder,
+            2 : spotify,
             8 : placeholder,
             9 : thumbs_up
             }
