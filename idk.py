2b90d5c2b4193079d579dd8d1cdeec499c45a826
Peter Mauldin <petermauldin@utexas.edu>
2015-03-01 02:07:54 -0600

    Added dependencies

---
+++ idk.py
@@ -0,0 +1,6 @@
+from pykeyboard import PyKeyboard
+
+k = PyKeyboard()
+k.press_key(k.alt_key)
+k.tap_key(k.tab_key)
+k.release_key(k.alt_key)
--- installCommands.sh
+++ installCommands.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+cd Dependencies/
 echo "Installing Snake-Solver Script..."
 sudo cp ./snake-solver /usr/bin
+echo "Done"
+echo "Installing dependency - python-xlib..."
+sudo apt-get install python-xlib
+sudo python setup.py build
+sudo python setup.py install
